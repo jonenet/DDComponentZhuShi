a8b4d723fc788689a365aebaf29bb0a95ca1ff98
jonenet <jonenet0619@163.com>
2018-03-28 22:22:06 +0800

    Initial commit

---
+++ sharecomponent/src/main/java/com/luojilab/share/applike/ShareApplike.java
@@ -0,0 +1,24 @@
+package com.luojilab.share.applike;
+
+import com.luojilab.component.componentlib.applicationlike.IApplicationLike;
+import com.luojilab.component.componentlib.router.ui.UIRouter;
+
+/**
+ * Created by mrzhang on 2017/6/15.
+ */
+
+public class ShareApplike implements IApplicationLike {
+
+    UIRouter uiRouter = UIRouter.getInstance();
+
+    //注册path
+    @Override
+    public void onCreate() {
+        uiRouter.registerUI("share");
+    }
+
+    @Override
+    public void onStop() {
+        uiRouter.unregisterUI("share");
+    }
+}
